b3b4332d9229681a757b7117233ba1bf296f8d55
HuitziDomingo <huitzitzilidomingo@gmail.com>
2022-12-26 18:40:39 -0600

    confirmacion de usuario agrgada

--- src/helpers/emails.js
+++ src/helpers/emails.js
@@ -1,5 +1,28 @@
 import nodemailer from 'nodemailer'
 
-export const emailRegister = (data) => {
+export const emailRegister =async (data) => {
     console.log('Datos: ',data)
+
+    let {email, name, token} = data
+
+    let transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: "96363c40d40b66",
+            pass: "4af715e04d471d"
+        }
+    })
+
+    //Informacion del email
+    let info =  await transport.sendMail({
+        from: '"Manager Tasker - Administrador de proyectos <cuentas@uptask.com>"',
+        to: email,
+        subject: "Up tasks - Comprueba tu cuenta",
+        text: 'Comprueba tu cuenta en Manager Tasker',
+        html: `
+            <p>Hola ${name} Comprueba tu cuenta.</p>
+            <a href="http://localhost:5173/confirmar/${token}">Comprobar Cuenta</a>
+        `
+    })
 }
